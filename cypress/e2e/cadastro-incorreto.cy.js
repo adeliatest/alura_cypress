091f17da363841802d4a5ad12c158d0cb5bb35c2
adeliadev <arodriguesthamyres@gmail.com>
2026-07-23 09:27:26 -0300

    refactor: refatora testes utilizando comandos personalizados

--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -1,15 +1,10 @@
 describe('Página de cadastro', () => {
   beforeEach(() => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/')
-  });
-  it('Deve preencher os campos do formulário incorretamente e exibir mensagens ao usuário', () => {
     // Abrir tela de cadastro - procura elemento com texto
     cy.get('[data-test="register-button"]').click();
-    // Clicar no botão de cadastro
-    cy.get('[data-test="submit-button"]').click();
-    // Validar mensagens de erro - Campos vazios
-    cy.contains('É necessário informar um endereço de email').should('be.visible')
-    cy.contains('Crie uma senha').should('be.visible')
-    cy.contains('Repita a senha criada acima').should('be.visible')
+  });
+  it('Deve preencher os campos do formulário incorretamente e exibir mensagens ao usuário', () => {
+    cy.cadastrar-incorreto('', '', '')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,42 @@
+Cypress.Commands.add('login', (email, senha) => {
+    // Selecionar campo de email
+    cy.get('[data-test="input-loginEmail"]').type(email);
+    // Selecionar campo de senha
+    cy.get('[data-test="input-loginPassword"]').type(senha);
+    // Clicar no botão Entrar
+    cy.get('[data-test="submit-button"]').click()
+})
+
+Cypress.Commands.add('cadastrar', (nome, email, senha) => {
+    // Selecionar campo nome
+    cy.get('[data-test="input-name"]').type(nome);
+    // Selecionar campo email
+    cy.get('[data-test="input-email"]').type(email);
+    // Selecionar campo senha
+    cy.get('[data-test="input-password"]').type(senha);
+    // Selecionar campo de confirmação de senha
+    cy.get('[data-test="input-confirm-password"]').type(senha);
+    // Clicar no botão de cadastro
+    cy.get('[data-test="submit-button"]').click();
+})
+
+Cypress.Commands.add('cadastrar-incorreto', (nome, email, senha) => {
+    // Selecionar campo nome
+    cy.get('[data-test="input-name"]').type(nome);
+    // Selecionar campo email
+    cy.get('[data-test="input-email"]').type(email);
+    // Selecionar campo senha
+    cy.get('[data-test="input-password"]').type(senha);
+    // Selecionar campo de confirmação de senha
+    cy.get('[data-test="input-confirm-password"]').type(senha);
+    // Clicar no botão de cadastro
+    cy.get('[data-test="submit-button"]').click();
+    // Validar mensagens de erro
+    cy.contains('É necessário informar um endereço de email').should('be.visible')
+    cy.contains('Crie uma senha').should('be.visible')
+    cy.contains('Repita a senha criada acima').should('be.visible')
+})
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
