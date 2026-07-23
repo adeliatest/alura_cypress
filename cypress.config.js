538d364c1094b17f7afdedb0d90881a61e905fc9
adeliadev <arodriguesthamyres@gmail.com>
2026-07-23 09:26:58 -0300

    config: adiciona id do cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '6svqix',
   e2e: {
     supportFile: 'cypress/support/e2e.js', 
   },
   // video: true,
-  reporter: 'mochawesome',
-  reporterOptions: {
-      reportDir: 'cypress/results',
-      overwrite: false,
-      html: true,
-      json: false,
-      timestamp: "mmddyyyy_HHMMss" }
+  // reporter: 'mochawesome',
+  // reporterOptions: {
+  //    reportDir: 'cypress/results',
+  //    overwrite: false,
+  //    html: true,
+  //    json: false,
+  //    timestamp: "mmddyyyy_HHMMss" }
 })
